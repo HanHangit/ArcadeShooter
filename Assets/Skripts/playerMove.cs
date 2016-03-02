2b9de18324641f27ab25f02872f5c03f03d8bd0a
12Cy <christian.sandkaemper@gmail.com>
2016-03-02 14:00:06 +0100

    player move

---
+++ Assets/Skripts/playerMove.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMove : MonoBehaviour {
+    //Public
+    public float fSpeed;
+
+    //Private
+    Rigidbody2D Rigid2D;
+    Vector2 vDirection;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+        Rigid2D = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        vDirection = Vector2.zero;
+        KeyBoardInPut();
+	 
+    }
+
+    void FixedUpdate()
+    {
+        Rigid2D.velocity = vDirection.normalized * fSpeed;
+    }
+
+    void KeyBoardInPut ()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            vDirection += new Vector2(0, 1);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            vDirection += new Vector2(-1, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            vDirection += new Vector2(0, -1);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            vDirection += new Vector2(1, 0);
+        }
+    }
+}
