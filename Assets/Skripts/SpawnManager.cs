7f7b639c90a7cb9eaa77b45998aa546410de57da
HanHangit <clonkcool@yahoo.de>
2016-03-02 15:47:02 +0100

    Added EnemySpawner

---
+++ Assets/Skripts/SpawnManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnManager : MonoBehaviour {
+    public float MinSpawnTime;
+    public float MaxSpawnTime;
+    public GameObject Enemy;
+
+    float Clock;
+    float RandomSpawnTime;
+	void Start()
+    {
+        RandomSpawnTime = Random.Range(MinSpawnTime, MaxSpawnTime);
+    }
+	// Update is called once per frame
+	void Update () {
+        Clock += Time.deltaTime;
+        if(Clock > RandomSpawnTime)
+        {
+            RandomSpawnTime = Random.Range(MinSpawnTime, MaxSpawnTime);
+            Instantiate(Enemy, transform.position, Quaternion.identity);
+            Clock = 0;
+        }
+	}
+}
