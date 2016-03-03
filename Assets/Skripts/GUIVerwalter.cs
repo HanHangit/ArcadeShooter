b3ac665a7066f598b985e0874ebb4f418e6d5b16
12Cy <christian.sandkaemper@gmail.com>
2016-03-03 12:32:35 +0100

    background hp etc

---
+++ Assets/Skripts/GUIVerwalter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIVerwalter : MonoBehaviour {
+
+    PlayerLife Leben;
+
+    float fLebensanzeige;
+
+
+	void Start () {
+        Leben = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerLife>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        fLebensanzeige = Leben.GetHp();
+	}
+    public float getLeben() {
+        return fLebensanzeige;
+    }
+}
---
+++ Assets/Skripts/HPTextAnzeige.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HPTextAnzeige : MonoBehaviour {
+
+    //Public
+   
+
+    //private
+    Text Anzeige;
+
+    GUIVerwalter AnzeigeFuerLeben;
+
+
+	// Use this for initialization
+	void Start () {
+        AnzeigeFuerLeben = GameObject.FindGameObjectWithTag("Player").GetComponent<GUIVerwalter>();
+        AnzeigeFuerLeben.getLeben();
+        //Anzeige = gameObject.GetComponent<Text>();
+        Debug.Log(AnzeigeFuerLeben.getLeben());
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Anzeige.text = "HPLebenhihi: "; //+ AnzeigeFuerLeben.ToString();
+        Anzeige.text.ToString();
+    }
+   
+}
